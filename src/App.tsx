d5eeadc7627663dc6e5217d46f5faaaf8dafec7f
valenabelen2005 <valen.abelen2005@gmail.com>
2026-04-07 23:12:26 +0200

    Auto-deploy: actualizar archivos

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+      <Analytics />
     </TooltipProvider>
   </QueryClientProvider>
 );
